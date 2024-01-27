131e6626c439c871739b694f9d9884699e0eda3d
Heck <33295627+Hecking-Heck@users.noreply.github.com>
2024-01-27 21:08:16 +0000

    Create index.py

---
+++ Basics/index.py
@@ -0,0 +1,25 @@
+# Variables
+varA = 33 # Int
+varC = "Hello" # String
+varD = 0.2 # Float
+
+# Hello World
+print("hello world!")
+
+# Arrays
+inventoryVar = ["apple", "banana", "pen", "pineapple"]
+citiesVar = ["Newcastle", "London", "Birmingham", "Leeds", "Liverpool"]
+
+print(citiesVar[1]) # This will print the 2nd member of the Array. (Note the Array index starts at 0)
+
+# IF Statement
+apples = 33
+bananas = 200
+if bananas > apples:
+  print("Bananas win!")
+
+# IF and Else
+if bananas > apples:
+  print("Bananas win!")
+else:
+  print("Apples Win!")
